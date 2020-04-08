73b6bbcd6cfe9ea746229cb63aa16cd001e111fe
Catherine Kim <catherinesjkim@gmail.com>
2020-04-08 06:12:53 -0700

    Add Updated queries SQL File Because: - Completed all Database Queries including the Stretches(2).

--- queries.sql
+++ queries.sql
@@ -22,10 +22,20 @@ FROM [Customers]
 WHERE City='London' OR City='Madrid' OR Country='Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-
+INSERT into Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE [Customers] 
+SET PostalCode = '11122'
+WHERE ContactName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT City
+FROM Customers
+GROUP BY City
+HAVING ( COUNT(City) = 1 );
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT * from Suppliers 
+WHERE length(SupplierName) > 20;
